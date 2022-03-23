efaac218a1745ac4b633ce895e3b458d8a9b7f46
breazzzy <billybarrese@comcast.net>
2022-03-23 16:17:41 -0400

    buy/sell models

---
+++ models/Buy.js
@@ -0,0 +1,28 @@
+module.exports = (sequeilize, DataTypes) => {
+    const Buy = sequeilize.define(
+      "Buy",
+      {
+        stockTicker: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        priceAtBuy:{
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        amountBought:{
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        }
+      },
+      {
+        modelName: "Buy",
+      }
+    );
+    return Buy;
+  };
+  
---
+++ models/Sell.js
@@ -0,0 +1,28 @@
+module.exports = (sequeilize, DataTypes) => {
+    const Sell = sequeilize.define(
+      "Sell",
+      {
+        stockTicker: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        priceAtSell:{
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        amountSold:{
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        }
+      },
+      {
+        modelName: "Sell",
+      }
+    );
+    return Sell;
+  };
+  
